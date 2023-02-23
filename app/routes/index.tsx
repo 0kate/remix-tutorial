eeb21b06d82abf5a21d5159789d67e15eeeee100
kate <kate@pop-os.localdomain>
2023-02-23 19:48:32 +0900

    add index loader

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,15 @@
+import { json } from '@remix-run/cloudflare';
+import { useLoaderData } from '@remix-run/react';
+
+export const loader = async () => {
+  return json({ ok: true });
+};
+
 export default function Index() {
+  const data = useLoaderData<typeof loader();
+
+  console.log(data);
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
